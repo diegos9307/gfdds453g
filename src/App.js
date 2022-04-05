8d6971834dddcf304772d647b242c8e842262f50
Diego A. Suaza R <diegos9307@gmail.com>
2022-04-05 09:52:43 -0500

    solucion problema

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
-import {
-  Switch,
-  Route,
-  Redirect,
-  Link
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
       <div>
         <nav>
-          {/*Aca deben ir los links de navegacion*/}
+          <Link to="/page1">Page 1</Link>
+          <Link to="/page2">Page 2</Link>
         </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+        <Switch>
+          {/* <Route exact path="/" /> */}
+          <Redirect exact from="/" to="/page1" />
+          <Route path="/page1" component={Page1} />
+          <Route path="/page2" component={Page2} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </div>
-    )
+    );
   }
 }
 
 // No tienes que hacer nada por debajo de esta linea
 //==================================================
 
-
 export const Page1 = (props) => {
-  return (
-    <h1>Pagina 1</h1>
-  )
-}
+  return <h1>Pagina 1</h1>;
+};
 
 export const Page2 = (props) => {
-  return (
-    <h1>Pagina 2</h1>
-  )
-}
+  return <h1>Pagina 2</h1>;
+};
 
 export const NotFound = (props) => {
-  return (
-    <h1>Pagina no encontrada</h1>
-  )
-}
+  return <h1>Pagina no encontrada</h1>;
+};
